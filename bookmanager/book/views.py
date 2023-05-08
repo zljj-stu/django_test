2bed2f4e26275cb352543c7087feefa432e141e2
zlj <807329951@qq.com>
2023-05-08 14:06:55 +0800

    定义视图函数

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpRequest
+from django.http import HttpResponse
 # Create your views here.
+'''
+视图函数有以下要求
+1、接受请求request(HttpRequest的类对象)
+2、必须返回响应response(HttpResponse的类对象)
+'''
+def index(request):
+    return HttpResponse('ok')
